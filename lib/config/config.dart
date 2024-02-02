4f23f4ce77c9effac11496373d68f90df4e03d02
shibilstark <shibilhassankmangode@gmail.com>
2024-02-02 17:37:26 +0530

    Added basic setup to config folder

---
+++ lib/config/config.dart
@@ -0,0 +1,6 @@
+export 'api/api.dart';
+export 'build_config/build_config.dart';
+export 'constants/enums.dart';
+export 'constants/preference_constants.dart';
+export 'styles/colors.dart';
+export 'styles/dimensions.dart';
---
+++ lib/config/styles/dimensions.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppFontSize {
+  static const double largeTitle = 22;
+  static const double smallTitle = 18;
+
+  static const double large = 16;
+  static const double medium = 14;
+  static const double small = 12;
+  static const double verySmall = 10;
+}
+
+class AppFontWeight {
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight normal = FontWeight.w400;
+  static const FontWeight bold = FontWeight.w500;
+}
+
+class AppPadding {
+  static const normalScreenPadding = EdgeInsets.all(10);
+  static const smallPadding = EdgeInsets.all(5);
+}
