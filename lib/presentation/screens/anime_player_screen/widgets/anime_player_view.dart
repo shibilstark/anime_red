b5f4fb4c328921308083a4e9bbfca9cfbaf94c8e
shibilstark <shibilhassankmangode@gmail.com>
2024-02-06 18:08:38 +0530

    Player Controles Customized

--- lib/presentation/screens/anime_player_screen/widgets/anime_player_view.dart
+++ lib/presentation/screens/anime_player_screen/widgets/anime_player_view.dart
@@ -1,3 +1,4 @@
+import 'package:anime_red/config/config.dart';
 import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -29,25 +30,33 @@ class _AnimePlayerViewState extends State<AnimePlayerView> {
       Uri.parse(videoId),
     );
 
-    chewieController = ChewieController(
-      videoPlayerController: videoPlayercontroller,
-      autoPlay: true,
-      aspectRatio: 16 / 9,
-      showOptions: false,
-      allowPlaybackSpeedChanging: true,
-      autoInitialize: true,
-      showControls: false,
-      customControls: const Row(
-        children: [
-          Icon(Icons.play_arrow),
-        ],
-      ),
-    );
+    chewieController = _getCustomContrller(videoPlayercontroller);
+
     super.initState();
+  }
+
+  ChewieController _getCustomContrller(
+      VideoPlayerController videoPlayerController) {
+    return ChewieController(
+        videoPlayerController: videoPlayerController,
+        autoPlay: true,
+        autoInitialize: true,
+        allowFullScreen: true,
+        aspectRatio: 16 / 9,
+        showControlsOnInitialize: false,
+        showControls: true,
+        allowedScreenSleep: false,
+        materialProgressColors: ChewieProgressColors(
+          backgroundColor: AppColors.grey,
+          bufferedColor: AppColors.silver,
+          playedColor: AppColors.white,
+          handleColor: AppColors.red,
+        ));
   }
 
   @override
   void dispose() {
+    chewieController.pause();
     videoPlayercontroller.dispose();
     chewieController.dispose();
     super.dispose();
@@ -55,34 +64,29 @@ class _AnimePlayerViewState extends State<AnimePlayerView> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AnimeBloc, AnimeState>(
-      builder: (context, state) {
-        if (state is AnimeSuccess) {
-          if (state.playerData != null) {
-            state.playerData!.fold((l) => null, (r) {
-              videoId = r.currentLink.url;
-              videoPlayercontroller = VideoPlayerController.networkUrl(
-                Uri.parse(videoId),
-              );
-              chewieController = ChewieController(
-                videoPlayerController: videoPlayercontroller,
-                autoPlay: true,
-                aspectRatio: 16 / 9,
-                showOptions: false,
-                allowPlaybackSpeedChanging: true,
-                autoInitialize: true,
-              );
-            });
-          }
-        }
+    return BlocListener<AnimeBloc, AnimeState>(
+        listener: (context, state) {
+          if (state is AnimeSuccess) {
+            if (state.playerData != null) {
+              chewieController.pause();
 
-        return AspectRatio(
+              state.playerData!.fold((l) => null, (r) {
+                videoId = r.currentLink.url;
+                videoPlayercontroller = VideoPlayerController.networkUrl(
+                  Uri.parse(videoId),
+                );
+                chewieController = _getCustomContrller(videoPlayercontroller);
+              });
+
+              chewieController.play();
+            }
+          }
+        },
+        child: AspectRatio(
           aspectRatio: 16 / 9,
           child: Chewie(
             controller: chewieController,
           ),
-        );
-      },
-    );
+        ));
   }
 }
