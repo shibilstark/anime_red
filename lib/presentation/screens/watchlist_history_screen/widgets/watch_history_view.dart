6e11a5b64e17628cc2813318c77fffa31d824c94
shibilstark <shibilhassankmangode@gmail.com>
2024-02-07 20:50:49 +0530

    Added Action button & Refactored Tile Widget

--- lib/presentation/screens/watchlist_history_screen/widgets/watch_history_view.dart
+++ lib/presentation/screens/watchlist_history_screen/widgets/watch_history_view.dart
@@ -33,162 +33,7 @@ class WatchHistoryViewWidget extends StatelessWidget {
         return Expanded(
           child: ListView.separated(
             shrinkWrap: true,
-            itemBuilder: (context, index) => ClipRRect(
-              borderRadius: BorderRadius.circular(5),
-              child: Stack(
-                children: [
-                  const Positioned.fill(
-                    child: SizedBox(
-                      width: double.infinity,
-                    ),
-                  ),
-                  Padding(
-                    padding: AppPadding.normalScreenPadding,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Container(
-                              width: context.screenWidth * 0.2,
-                              decoration: BoxDecoration(
-                                  color: AppColors.grey,
-                                  borderRadius: BorderRadius.circular(5)),
-                              child: AspectRatio(
-                                aspectRatio: 3 / 4,
-                                child: Stack(
-                                  children: [
-                                    Positioned.fill(
-                                      child: Image.asset(
-                                        AppImageAssets.landgingBg,
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                    const Align(
-                                      alignment: Alignment.bottomCenter,
-                                      child: LinearProgressIndicator(
-                                        color: AppColors.red,
-                                        backgroundColor: AppColors.grey,
-                                        value: 0.75,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ),
-                            const Gap(W: 10),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const Text(
-                                    "Itach Uchiha - The Legend of Village",
-                                    overflow: TextOverflow.clip,
-                                    style: TextStyle(
-                                      color: AppColors.white,
-                                      fontSize: AppFontSize.smallTitle,
-                                      fontWeight: AppFontWeight.bold,
-                                    ),
-                                  ),
-                                  const Gap(H: 5),
-                                  const Wrap(
-                                    alignment: WrapAlignment.start,
-                                    children: [
-                                      Icon(
-                                        CupertinoIcons.play_fill,
-                                        color: AppColors.indicator,
-                                        size: 18,
-                                      ),
-                                      Gap(W: 10),
-                                      Text(
-                                        "EP 148",
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
-                                        style: TextStyle(
-                                          color: AppColors.red,
-                                          fontWeight: AppFontWeight.normal,
-                                          fontSize: AppFontSize.medium,
-                                        ),
-                                      ),
-                                      Gap(W: 10),
-                                      Text(
-                                        "Continue watching",
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
-                                        style: TextStyle(
-                                          color: AppColors.indicator,
-                                          fontWeight: AppFontWeight.normal,
-                                          fontSize: AppFontSize.medium,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  const Gap(H: 10),
-                                  Container(
-                                    padding: const EdgeInsets.symmetric(
-                                      vertical: 2,
-                                      horizontal: 6,
-                                    ),
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(3),
-                                      color: AppColors.white,
-                                    ),
-                                    child: const Text(
-                                      "SUB",
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                        color: AppColors.red,
-                                        fontWeight: AppFontWeight.bolder,
-                                        fontSize: AppFontSize.verySmall,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                        const Gap(H: 20),
-                        Wrap(
-                          alignment: WrapAlignment.start,
-                          spacing: 10,
-                          runSpacing: 20,
-                          children: List.generate(
-                            ["Romance", "Action", "Adventure", "Fantasy"]
-                                .length,
-                            (index) => Container(
-                              decoration: BoxDecoration(
-                                  color: AppColors.red,
-                                  borderRadius: BorderRadius.circular(3)),
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 2,
-                                horizontal: 6,
-                              ),
-                              child: Text(
-                                [
-                                  "Romance",
-                                  "Action",
-                                  "Adventure",
-                                  "Fantasy"
-                                ][index],
-                                style: const TextStyle(
-                                  color: AppColors.white,
-                                  fontWeight: AppFontWeight.bold,
-                                  fontSize: AppFontSize.small,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            itemBuilder: (context, index) => const WatchHistoryTileWidget(),
             separatorBuilder: (context, index) => const Divider(
               thickness: 0.4,
               color: AppColors.grey,
@@ -197,6 +42,196 @@ class WatchHistoryViewWidget extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+}
+
+class WatchHistoryTileWidget extends StatelessWidget {
+  const WatchHistoryTileWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(5),
+      child: Stack(
+        children: [
+          const Positioned.fill(
+            child: SizedBox(
+              width: double.infinity,
+            ),
+          ),
+          Padding(
+            padding: AppPadding.normalScreenPadding,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      width: context.screenWidth * 0.2,
+                      decoration: BoxDecoration(
+                          color: AppColors.grey,
+                          borderRadius: BorderRadius.circular(5)),
+                      child: AspectRatio(
+                        aspectRatio: 3 / 4,
+                        child: Stack(
+                          children: [
+                            Positioned.fill(
+                              child: Image.asset(
+                                AppImageAssets.landgingBg,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            const Align(
+                              alignment: Alignment.bottomCenter,
+                              child: LinearProgressIndicator(
+                                color: AppColors.red,
+                                backgroundColor: AppColors.grey,
+                                value: 0.75,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    const Gap(W: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Itach Uchiha - The Legend of Village",
+                            overflow: TextOverflow.clip,
+                            style: TextStyle(
+                              color: AppColors.white,
+                              fontSize: AppFontSize.smallTitle,
+                              fontWeight: AppFontWeight.bold,
+                            ),
+                          ),
+                          const Gap(H: 5),
+                          const Wrap(
+                            alignment: WrapAlignment.start,
+                            children: [
+                              Icon(
+                                CupertinoIcons.play_fill,
+                                color: AppColors.indicator,
+                                size: 18,
+                              ),
+                              Gap(W: 10),
+                              Text(
+                                "EP 148",
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  color: AppColors.red,
+                                  fontWeight: AppFontWeight.normal,
+                                  fontSize: AppFontSize.medium,
+                                ),
+                              ),
+                              Gap(W: 10),
+                              Text(
+                                "Continue watching",
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  color: AppColors.indicator,
+                                  fontWeight: AppFontWeight.normal,
+                                  fontSize: AppFontSize.medium,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Gap(H: 10),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 2,
+                              horizontal: 6,
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(3),
+                              color: AppColors.white,
+                            ),
+                            child: const Text(
+                              "SUB",
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: AppColors.red,
+                                fontWeight: AppFontWeight.bolder,
+                                fontSize: AppFontSize.verySmall,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const Gap(H: 20),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Wrap(
+                        alignment: WrapAlignment.start,
+                        spacing: 10,
+                        runSpacing: 20,
+                        children: List.generate(
+                          ["Romance", "Action", "Adventure", "Fantasy"].length,
+                          (index) => Container(
+                            decoration: BoxDecoration(
+                                color: AppColors.red,
+                                borderRadius: BorderRadius.circular(3)),
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 2,
+                              horizontal: 6,
+                            ),
+                            child: Text(
+                              [
+                                "Romance",
+                                "Action",
+                                "Adventure",
+                                "Fantasy"
+                              ][index],
+                              style: const TextStyle(
+                                color: AppColors.white,
+                                fontWeight: AppFontWeight.bold,
+                                fontSize: AppFontSize.small,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Gap(W: 10),
+                    Container(
+                      width: 25,
+                      height: 25,
+                      padding: const EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(3),
+                          border: Border.all(
+                            width: 1,
+                            color: AppColors.grey,
+                          )),
+                      child: const FittedBox(
+                        child: Icon(
+                          Icons.delete,
+                          color: AppColors.white,
+                          size: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
